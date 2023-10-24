6c1ad83cc61b3a6d2e4838dee68d0764caafd373
chrisleitzinger <christelleleitzinger@gmail.com>
2023-10-24 17:22:00 -0400

    Finish cleaning and create variable for criteria admission

--- R/01.data cleaning.R
+++ R/01.data cleaning.R
@@ -7,8 +7,8 @@ library(lubridate)
 path <- fs::path("", "Volumes", "Gillis_Research","Christelle Colin-Leitzinger", "adiposity_lymphoma_ch")
 
 scans_data <- 
-  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20230816_outfile.xlsx"),
-                    sheet = "PET_CT SCANS"
+  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20231010_outfile_updated.xlsx"),
+                    sheet = "PET_CT scans"
   ) %>% 
   janitor::clean_names() %>% 
   mutate(mrn = as.character(mrn))
@@ -19,21 +19,21 @@ scans_type <-
   janitor::clean_names()
 
 dna_data <- 
-  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20230816_outfile.xlsx"),
+  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20231010_outfile_updated.xlsx"),
                     sheet = "BioBanking Data"
   ) %>% 
   janitor::clean_names() %>% 
   mutate(mrn = as.character(mrn))
 
 clinical <- 
-  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20230816_outfile.xlsx"),
+  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20231010_outfile_updated.xlsx"),
                     sheet = "Cancer Registry"
   ) %>% 
   janitor::clean_names() %>% 
   mutate(mrn = as.character(mrn))
 
 weight <- 
-  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20230816_outfile.xlsx"),
+  readxl::read_xlsx(paste0(here::here(), "/10R23000188_20231010_outfile_updated.xlsx"),
                     sheet = "HT_WT "
   ) %>% 
   janitor::clean_names() %>% 
@@ -41,12 +41,13 @@ weight <-
 
 
 ################################################################################# II ### Data cleaning
-# Scans
+# Get body area being scanned  
 scans_type_contrast <- scans_type %>% 
   filter(ideal_cohort_c_ts_post_contrast_only == 1)
 scans_type_petct <- scans_type %>% 
   filter(ideal_cohort_pet_ct_only == 1)
 
+# Scans # Make a contrast and non contrast data to find samples for each dataset
 scans_contrast <- scans_data %>% 
   filter(charge_service_desc %in% c(scans_type_contrast$charge_service_desc)) %>% 
   select(mrn, contrast_scans_date = service_dt,  contrast_imaging_types = charge_service_desc)
@@ -89,7 +90,7 @@ germline_dna <- dna_data %>%
   arrange(mrn, interval_blood_tx) %>% 
   distinct(mrn, .keep_all = TRUE) %>% 
   filter(!is.na(interval_blood_tx)) %>% 
-  select(collection_dt_germline = specimen_collection_dt,
+  select(mrn, collection_dt_germline = specimen_collection_dt,
          sample_type_germline = sample_type, 
          sample_family_id_germline = sample_family_id,
          sample_id_germline = sample_id,
@@ -104,8 +105,72 @@ clinical <- clinical %>%
   mutate(across(where(is.character), ~ na_if(., "Na")))
 
 # Weight and height - select value before and closest to tx 
+height <- weight %>% 
+  filter(!is.na(vitals_value_height)) %>% 
+  select(mrn, treatment_start_dt, height = vitals_value_height, 
+         height_unit = vitals_units_in, height_date = vitals_dtm) %>% 
+  mutate(time_height_tx_days = case_when(
+    height_date <= treatment_start_dt                   ~ interval(
+      start = height_date, end = treatment_start_dt) /
+      duration(n=1, unit = "days")
+  )) %>% 
+  filter(!is.na(height)) %>% 
+  arrange(mrn, time_height_tx_days) %>% 
+  distinct(mrn, .keep_all = TRUE)
+
+weight <- weight %>% 
+  filter(!is.na(vitals_value_weight)) %>% 
+  select(mrn, treatment_start_dt, weight = vitals_value_weight, 
+         weight_unit = vitals_units_lbs, weight_date = vitals_dtm, 
+         time_weight_tx_days = days_between_vitals_dt_tx_dt) %>% 
+  mutate(time_weight_tx_days = case_when(
+    weight_date <= treatment_start_dt                   ~ interval(
+      start = weight_date, end = treatment_start_dt) /
+      duration(n=1, unit = "days")
+  )) %>% 
+  # select(-treatment_start_dt) %>% 
+  filter(!is.na(weight)) %>% 
+  arrange(mrn, time_weight_tx_days) %>% 
+  distinct(mrn, .keep_all = TRUE)
+
+bmi <- full_join(weight, height,
+                 by = c("mrn", "treatment_start_dt")) %>% 
+  filter(!is.na(weight) & !is.na(height))
 
 
+################################################################################# III ### Merge data
+lymphoma_data <- clinical %>% 
+  inner_join(., germline_dna,
+             by = "mrn") %>% 
+  inner_join(., bmi,
+             by = "mrn")
+
+write_rds(lymphoma_data, "lymphoma data.rds")
+
+rm(clinical, dna_data, germline_dna,
+   height, weight, bmi)
 
 
+################################################################################# III ### Create variables
+lymphoma_data <- read_rds(paste0(here::here(), "/lymphoma data.rds"))
 
+lymphoma_data <- lymphoma_data %>% 
+  mutate(weight_kg = weight / 2.205,
+         height_m = height / 39.37,
+         bmi = weight_kg / (height_m * height_m)) %>%
+  mutate(bmi_cat = case_when(
+    bmi < 25                    ~ "Underweight and normal weight",
+    bmi >= 25 &
+      bmi < 30                  ~ "Overweight",
+    bmi >= 30                   ~ "Obese"
+  )) %>%
+  mutate(bmi_cat = factor(bmi_cat, levels = c("Underweight and normal weight", "Overweight", "Obese"))) #%>% 
+  # mutate(os_time_months = interval(
+  #   start = first_treatment_dt, end = date) /
+  #     duration(n=1, unit = "months"))
+  
+
+write_rds(lymphoma_data, "lymphoma data with new variables.rds")
+
+
+################################################################################# IV ### Find patients fitting criteria
