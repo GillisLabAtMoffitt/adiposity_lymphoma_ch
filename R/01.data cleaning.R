f6279d2d76f3664483e451363a6c9b9e216e4102
chrisleitzinger <christelleleitzinger@gmail.com>
2024-01-30 10:57:51 -0500

    Update cleaning and patient list

--- R/01.data cleaning.R
+++ R/01.data cleaning.R
@@ -114,12 +114,12 @@ height <- weight %>%
          height_unit = vitals_units_in, height_date) %>% 
   mutate(time_height_tx_days = case_when(
     height_date <= treatment_start_dt                   ~ interval(
-      start = height_date, end = treatment_start_dt) /
+      start = height_date, end = first_treatment_dt) /
       duration(n=1, unit = "days")
   )) %>% 
   mutate(time_height_after_tx_days = case_when(
     treatment_start_dt < height_date                   ~ interval(
-      start = height_date, end = treatment_start_dt) /
+      start = height_date, end = first_treatment_dt) /
       duration(n=1, unit = "days")
   )) %>% 
   filter(!is.na(height)) %>% 
@@ -133,12 +133,12 @@ weight <- weight %>%
          weight_unit = vitals_units_lbs, weight_date) %>% 
   mutate(time_weight_tx_days = case_when(
     weight_date <= treatment_start_dt                   ~ interval(
-      start = weight_date, end = treatment_start_dt) /
+      start = weight_date, end = first_treatment_dt) /
       duration(n=1, unit = "days")
   )) %>% 
   mutate(time_weight_after_tx_days = case_when(
     treatment_start_dt < weight_date                   ~ interval(
-      start = weight_date, end = treatment_start_dt) /
+      start = weight_date, end = first_treatment_dt) /
       duration(n=1, unit = "days")
   )) %>% 
   # select(-treatment_start_dt) %>% 
@@ -200,13 +200,13 @@ lymphoma_data_petct <- lymphoma_data_petct %>%
     TRUE                                                ~ "No"
   )) %>% 
   mutate(time_petct_tx_days = interval(
-      start = petct_scans_date, end = treatment_start_dt) /
+      start = petct_scans_date, end = first_treatment_dt) /
       duration(n=1, unit = "days")
   ) %>% 
   # select(mrn,petct_scans_date, petct_bf_tx, treatment_start_dt, time_petct_tx_days) %>% 
-  filter(petct_bf_tx == "Scan before") %>% 
-  arrange(mrn, time_petct_tx_days) %>% 
-  distinct(mrn, .keep_all = TRUE)
+  # filter(petct_bf_tx == "Scan before") %>% 
+  # arrange(mrn, time_petct_tx_days) %>% 
+  # distinct(mrn, .keep_all = TRUE)
 
 table(lymphoma_data_petct$bmi_cat)
 write_rds(lymphoma_data_petct, paste0("lymphoma_data_petct_", today(), ".rds"))
@@ -222,7 +222,7 @@ lymphoma_data_contrast <- lymphoma_data_contrast %>%
     TRUE                                                ~ "No"
   )) %>% 
   mutate(time_contrast_tx_days = interval(
-      start = contrast_scans_date, end = treatment_start_dt) /
+      start = contrast_scans_date, end = first_treatment_dt) /
       duration(n=1, unit = "days")
   ) %>% 
   # select(mrn,contrast_scans_date, contrast_bf_tx, treatment_start_dt, time_contrast_tx_days) %>% 
